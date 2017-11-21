88e4b742a514871238d2e67f166601f6e73d5131
kinglong <1406733029@qq.com>
2017-11-21 11:37:10 +0800

    1.6添加注释和log

--- assets/cases/collider/Shooter/Bullet.js
+++ assets/cases/collider/Shooter/Bullet.js
@@ -1,3 +1,6 @@
+/*
+*   被射手发射的子弹
+* */
 cc.Class({
     extends: cc.Component,
 
@@ -7,10 +10,15 @@ cc.Class({
 
     // use this for initialization
     onLoad: function () {
-
+        cc.log(cc.director.getCollisionManager().enabled,
+            cc.director.getCollisionManager().enabledDebugDraw,
+            cc.director.getCollisionManager().enabledDrawBoundingBox)
+        //碰撞检测开关来自父类的开关
     },
 
     onCollisionEnter: function (other, self) {
+        //子弹与砖块和顶部墙面碰撞platform, wall, 自己是bullet
+        cc.log(other.node.name, self.node.name);
         this.node.destroy();
     },
     
