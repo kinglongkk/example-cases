63eefef015ebd766304920674aeb2a7a20d720e5
kinglong <1406733029@qq.com>
2017-11-21 15:28:40 +0800

    1.6添加注释和log

--- assets/cases/collider/Shape/ShowCollider.js
+++ assets/cases/collider/Shape/ShowCollider.js
@@ -20,9 +20,11 @@ cc.Class({
     
     onBtnClick: function (event) {
         var target = event.target;
+        //`cc.${target.name}Collider` ES6字符串组成语法
         var shapeClassName = `cc.${target.name}Collider`;
         var nodePath = 'Canvas/root/' + target.parent.name;
         var collider = cc.find(nodePath).getComponent(shapeClassName);
+        cc.log("获取点击按钮的父类节点", nodePath);
         collider.enabled = !collider.enabled;
         
         var label = target.getChildByName('Label').getComponent(cc.Label);
